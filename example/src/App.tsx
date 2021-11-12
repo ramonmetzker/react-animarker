fc307b5f2c02f455e83a53e350d208fd22be43c4
Ramon Metzker <ramonmetzker12@gmail.com>
2021-11-12 02:10:51 -0400

    feat: create example page

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,33 +1,72 @@
 import React from 'react'
 
-import { Mark } from 'react-animarker'
+import { Mark, MarkString } from 'react-animarker'
+import Footer from './components/Footer'
+
+import Header from './components/Header'
+
+const MyJsxString = `
+<p>This example comes from a string written in an WYSIWYG editor</p>
+<p>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores deserunt,
+  voluptatibus modi ipsam odio, voluptatum accusamus deleniti aperiam obcaecati
+  recusandae necessitatibus officiis illo excepturi a magnam? Dicta illum dolor
+  neque.
+</p>
+<p>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente voluptate{' '}
+  <mark>ipsa optio rem dignissimos atque ut natus, officiis nulla quaerat!</mark>
+  Nam maxime esse non! Doloribus ratione necessitatibus consequuntur magnam
+  minus.
+</p>
+<p>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores deserunt,
+  voluptatibus modi ipsam odio, voluptatum accusamus deleniti aperiam obcaecati
+  recusandae necessitatibus officiis illo excepturi a magnam? Dicta illum dolor
+  neque.
+</p>
+`
 
 const App = () => {
   return (
-    <div style={{ marginTop: '1000px' }}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad mollitia
-      itaque quisquam suscipit dicta optio minima error saepe atque recusandae
-      quos eum illo voluptatibus harum ea culpa, veniam hic nesciunt? Lorem
-      ipsum dolor sit amet consectetur, adipisicing elit. Iste quasi mollitia
-      placeat itaque voluptates animi asperiores ab eveniet deleniti ut magni,
-      sapiente ad exercitationem minima rerum nostrum tempora cupiditate
-      consequatur! Maxime ex nulla{' '}
-      <Mark>
-        error perferendis dolore veritatis soluta, sit delectus magnam sunt
-        laudantium laborum, quas enim iusto voluptas dolorem deserunt doloribus
-        quod accusantium veniam asperiores! Optio cumque animi provident aut? Id
-        dolore deserunt consequuntur architecto corporis repellendus
-      </Mark>{' '}
-      necessitatibus tenetur ut earum sit, expedita praesentium nulla similique
-      voluptatem dolorum quasi dignissimos eveniet! Ipsam magnam consectetur
-      explicabo molestiae, similique suscipit incidunt! Officia. Sed, neque
-      dicta! Amet temporibus aliquid quas culpa vero unde blanditiis,
-      necessitatibus ratione ad maxime dolorum corrupti dicta quasi molestiae
-      quam, a, illum ipsum vel beatae doloribus nihil magni esse. Iusto, magnam
-      dolore praesentium amet at omnis incidunt repudiandae qui in aliquam
-      temporibus accusamus rem fugit distinctio sunt ipsa totam optio libero
-      suscipit impedit. Facilis exercitationem ex repellendus aliquid amet!
-    </div>
+    <>
+      <Header />
+      <section className='scroll'>
+        <h2>Scroll down</h2>
+      </section>
+      <h3>
+        <Mark color='#f73333' duration={4}>
+          This is an animarker's Mark component
+        </Mark>
+      </h3>
+      <h3 style={{ color: 'white' }}>
+        <Mark color='#4444ff' duration={2}>
+          This is another animarker's Mark component
+        </Mark>
+      </h3>
+      <div className='text'>
+        <p>This is an example of a text with a Mark inside</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+          deserunt, voluptatibus modi ipsam odio, voluptatum accusamus deleniti
+          aperiam obcaecati recusandae necessitatibus officiis illo excepturi a
+          magnam? Dicta illum dolor neque.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+          voluptate{' '}
+          <Mark color='rgba(68, 255, 130, 0.5)'>
+            ipsa optio rem dignissimos atque ut natus, officiis nulla quaerat!
+          </Mark>{' '}
+          Nam maxime esse non! Doloribus ratione necessitatibus consequuntur
+          magnam minus.
+        </p>
+      </div>
+      <div className='text'>
+        <MarkString jsx={MyJsxString} renderInWrapper={false} />
+      </div>
+      <Footer />
+    </>
   )
 }
 
