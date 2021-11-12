75265dcbeaf87077c7ab0f896059b66937fc20dc
Ramon Metzker <ramonmetzker12@gmail.com>
2021-11-12 00:47:16 -0400

    feat: add margin to visualize the example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,20 +11,22 @@ const App = () => {
       ipsum dolor sit amet consectetur, adipisicing elit. Iste quasi mollitia
       placeat itaque voluptates animi asperiores ab eveniet deleniti ut magni,
       sapiente ad exercitationem minima rerum nostrum tempora cupiditate
-      consequatur! Maxime ex nulla <Mark>error</Mark> perferendis dolore
-      veritatis soluta, sit delectus magnam sunt laudantium laborum, quas enim
-      iusto voluptas dolorem deserunt doloribus quod accusantium veniam
-      asperiores! Optio cumque animi provident aut? Id dolore deserunt
-      consequuntur architecto corporis repellendus necessitatibus tenetur ut
-      earum sit, expedita praesentium nulla similique voluptatem dolorum quasi
-      dignissimos eveniet! Ipsam magnam consectetur explicabo molestiae,
-      similique suscipit incidunt! Officia. Sed, neque dicta! Amet temporibus
-      aliquid quas culpa vero unde blanditiis, necessitatibus ratione ad maxime
-      dolorum corrupti dicta quasi molestiae quam, a, illum ipsum vel beatae
-      doloribus nihil magni esse. Iusto, magnam dolore praesentium amet at omnis
-      incidunt repudiandae qui in aliquam temporibus accusamus rem fugit
-      distinctio sunt ipsa totam optio libero suscipit impedit. Facilis
-      exercitationem ex repellendus aliquid amet!
+      consequatur! Maxime ex nulla{' '}
+      <Mark>
+        error perferendis dolore veritatis soluta, sit delectus magnam sunt
+        laudantium laborum, quas enim iusto voluptas dolorem deserunt doloribus
+        quod accusantium veniam asperiores! Optio cumque animi provident aut? Id
+        dolore deserunt consequuntur architecto corporis repellendus
+      </Mark>{' '}
+      necessitatibus tenetur ut earum sit, expedita praesentium nulla similique
+      voluptatem dolorum quasi dignissimos eveniet! Ipsam magnam consectetur
+      explicabo molestiae, similique suscipit incidunt! Officia. Sed, neque
+      dicta! Amet temporibus aliquid quas culpa vero unde blanditiis,
+      necessitatibus ratione ad maxime dolorum corrupti dicta quasi molestiae
+      quam, a, illum ipsum vel beatae doloribus nihil magni esse. Iusto, magnam
+      dolore praesentium amet at omnis incidunt repudiandae qui in aliquam
+      temporibus accusamus rem fugit distinctio sunt ipsa totam optio libero
+      suscipit impedit. Facilis exercitationem ex repellendus aliquid amet!
     </div>
   )
 }
